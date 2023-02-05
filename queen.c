354e91c4e1d5cae244e375d32ab811c7b7297e91
AfricanSwallow <shrubbery@gapp.nthu.edu.tw>
2023-02-05 21:57:59 +0800

    Is functional

--- queen.c
+++ queen.c
@@ -3,59 +3,99 @@
 #include <string.h>
 #include <stdbool.h>
 #include <limits.h>
-int n;
+int N, M, total;
 int *diff;
 int *sum;
-bool *visit_col;
-int sol_cnt = 0, diff_cnt = 0;
+bool *visit_col, *isQueen;
+int sol_cnt = 0, put_cnt = 0, N_cnt, M_cnt;
 
 void Visit(int row);
-bool valid(int row, int column);
+bool valid(int row, int column, bool IsQueen);
 void Reset(void);
 
 int main(void) {
-    scanf("%d", &n);
-    diff = (int*) malloc(sizeof(int)*n);
-    sum = (int*) malloc(sizeof(int)*n);
-    visit_col = (bool*) malloc(sizeof(bool)*n);
-    Reset();
-    Visit(0);
-    printf("%d solution\n", sol_cnt);
-
+    while(scanf("%d%d", &N, &M) == 2) {
+        total = N + M;
+        N_cnt = N;
+        M_cnt = M;
+        diff = (int*) malloc(sizeof(int)*total);
+        sum = (int*) malloc(sizeof(int)*total);
+        visit_col = (bool*) malloc(sizeof(bool)*total);
+        isQueen = (bool*) malloc(sizeof(bool)*total);
+        Reset();
+        Visit(0);
+        printf("%d\n", sol_cnt);
+    }
+    
     free(diff);
     free(sum);
     free(visit_col);
+    free(isQueen);
 }
 
 void Visit(int row) {
-    if(row == n) {
+    if(row == total) {
         sol_cnt++;
-        printf("Found\n");
+        // printf("Found\n");
         return;
     }
 
+    for(int j = 0; j < 2; j++) {
+        for(int i = 0; i < total; i++) {
+            if(valid(row, i, j)) {
+                visit_col[i] = true;
+                diff[put_cnt] = row - i;
+                sum[put_cnt] = row + i;
+                if(j == 0) {
+                    // printf("Rook visit (%d, %d)\n", row, i);
+                    M_cnt--;
+                }
+                else {
+                    // printf("Queen visit (%d, %d)\n", row, i);
+                    isQueen[put_cnt] = true;
+                    N_cnt--;
+                }
+                put_cnt++;
+                
+                Visit(row+1);
 
-    for(int i = 0; i < n; i++) {
-        if(valid(row, i)) {
-            visit_col[i] = true;
-            diff[diff_cnt] = row - i;
-            sum[diff_cnt++] = row + i;
-            printf("visit (%d, %d)\n", row, i);
-            Visit(row+1);
-            visit_col[i] = false;
-            diff[--diff_cnt] = INT_MAX;
-            sum[diff_cnt] = INT_MAX;
+                //Restore tracking
+                visit_col[i] = false;
+                put_cnt--;
+                diff[put_cnt] = INT_MAX;
+                sum[put_cnt] = INT_MAX;
+                if(j == 0)
+                    M_cnt++;
+                else {
+                    isQueen[put_cnt] = false;
+                    N_cnt++;
+                }
+            }
         }
     }
+
+    
 }
 
-bool valid(int row, int column) {
+bool valid(int row, int column, bool ThisIsQueen) {
     int difference = row - column;
     int summation = row + column;
+    //See if there are enough queens or rooks
+    if(ThisIsQueen) {
+        if(N_cnt == 0)
+            return false;
+    }else
+        if(M_cnt == 0)
+            return false;
+
     if(visit_col[column] == false) {
-        for(int i = 0; i < diff_cnt; i++) {
-            if(difference == diff[i] || summation == sum[i]) {
-                return false;
+        for(int i = 0; i < put_cnt; i++) {
+            if(ThisIsQueen) {
+                if(difference == diff[i] || summation == sum[i])
+                    return false;
+            }else {
+                if((difference == diff[i] || summation == sum[i]) && isQueen[i]) 
+                    return false;
             }
         }
         return true;
@@ -64,7 +104,10 @@ bool valid(int row, int column) {
 }
 
 void Reset(void) {
-    memset(diff, INT_MAX, sizeof(int)*n);
-    memset(sum, INT_MAX, sizeof(int)*n);
-    memset(visit_col, false, sizeof(bool)*n);
+    memset(diff, INT_MAX, sizeof(int)*total);
+    memset(sum, INT_MAX, sizeof(int)*total);
+    memset(visit_col, false, sizeof(bool)*total);
+    memset(isQueen, false, sizeof(bool)*total);
+    sol_cnt = 0;
+    put_cnt = 0;
 }
